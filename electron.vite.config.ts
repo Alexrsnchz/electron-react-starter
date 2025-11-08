2fe228d62e30ff1c6e4d43dd83c1e134e5cce6c8
Alexrsnchz <alexrsnchz@gmail.com>
2025-11-08 23:40:34 +0100

    chore: Add template files

---
+++ electron.vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'electron-vite';
+
+export default defineConfig({
+    main: {
+        build: {
+            lib: { entry: 'src/main/index.ts' }
+        },
+    },
+    preload: {
+        build: {
+            lib: { entry: 'src/preload/index.ts' }
+        },
+    },
+    renderer: {
+        plugins: [],
+    }
+});
