9353d633effb9ede9a80e1d57599eaee9aba9068
Alexrsnchz <alexrsnchz@gmail.com>
2025-11-12 23:08:30 +0100

    refactor: Change of format

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "electron-vite";
+import { defineConfig } from 'electron-vite';
 
 export default defineConfig({
   main: {
     build: {
-      lib: { entry: "src/main/index.ts" },
+      lib: { entry: 'src/main/index.ts' },
     },
   },
   preload: {
     build: {
-      lib: { entry: "src/preload/index.ts" },
+      lib: { entry: 'src/preload/index.ts' },
     },
   },
   renderer: {
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,6 @@
-import { fileURLToPath } from "url";
-import path from "path";
-import { app, BrowserWindow } from "electron";
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { app, BrowserWindow } from 'electron';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,7 +10,7 @@ async function createWindow() {
     width: 900,
     height: 700,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
+      preload: path.join(__dirname, 'preload.js'),
     },
   });
 
@@ -18,23 +18,23 @@ async function createWindow() {
     if (process.env.VITE_DEV_SERVER_URL) {
       await win.loadURL(process.env.VITE_DEV_SERVER_URL);
     } else {
-      await win.loadFile(path.join(__dirname, "../dist/index.html"));
+      await win.loadFile(path.join(__dirname, '../dist/index.html'));
     }
   } catch (err) {
-    console.error("Error loading screen:", err);
+    console.error('Error loading screen:', err);
   }
 }
 
 app.whenReady().then(() => {
   createWindow().catch((err) => console.error(err));
 
-  app.on("activate", () => {
+  app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow().catch((err) => console.error(err));
     }
   });
 });
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
 });
